8aea995694f7c8718f713272d33ff26e71c2c500
FerrandoCarlos <cferrando2015@gmail.com>
2024-03-21 19:29:13 -0300

    Commit Inicial tp Aulico
    creacion de clases Baraja y Carta

---
+++ src/juego_de_cartaslab1/Baraja.java
@@ -0,0 +1,20 @@
+
+package juego_de_cartaslab1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ferrando Carlos
+ */
+public class Baraja {
+    private List <Carta> cartas;
+    private List <Carta> monton;
+
+    public Baraja() {
+        this.cartas = new ArrayList<>();
+        this.monton = new ArrayList<>();
+    }
+    
+    
+}
