e8683e1de2ff5ac364cf77aafddd0f76475b14d1
aguz11 <agus110196@gmail.com>
2024-03-21 19:56:30 -0300

    Update Baraja.java

--- src/juego_de_cartaslab1/Baraja.java
+++ src/juego_de_cartaslab1/Baraja.java
@@ -9,13 +9,15 @@ import java.util.List;
  * @author Ferrando Carlos
  */
 public class Baraja {
-    private List <Carta> cartas;
-    private List <Carta> monton;
+    private List<Carta> cartas;
+    private List<Carta> monton;
 
     public Baraja() {
         this.cartas = new ArrayList<>();
         this.monton = new ArrayList<>();
+        inicializarBaraja();
     }
+<<<<<<< HEAD
     
     public Carta siguienteCarta(){
     if (!cartas.isEmpty()) {
@@ -34,6 +36,33 @@ public class Baraja {
             System.out.println(carta);
         }
     }
+=======
+
+    /* Inicializa la baraja de manera secuencial */
+    private void inicializarBaraja() {
+        String[] palos = { "Espadas", "Bastos", "Oros", "Copas" };
+        for (int numero = 1; numero <= 12; numero++) {
+            if (numero != 8 && numero != 9) {
+                for (String palo : palos) {
+                    cartas.add(new Carta(numero, palo));
+                }
+            }
+        }
+    }
+
+    /* Solo un metodo de prueba para que muestra todas las cartas */
+    public void mostrarPrueba() {
+        int cant = 0;
+        for (Carta cart : cartas) {
+            System.out.println(cart);
+            cant++;
+        }
+        System.out.println("Cantidad:" + cant);
+
+    }
+    // cartanMonton
+
+>>>>>>> 642be465c946e5db5b4909905d99aa4f884fc302
 }
     
 
