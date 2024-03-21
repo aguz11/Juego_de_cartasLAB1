d8a9ebf194f860a539995b73ea45283ecf30daee
mpirizdutra89 <163307010+mpirizdutra89@users.noreply.github.com>
2024-03-21 19:42:04 -0300

    Metodo inicializarBaraja y mostrarPrueba

--- src/juego_de_cartaslab1/Baraja.java
+++ src/juego_de_cartaslab1/Baraja.java
@@ -8,13 +8,33 @@ import java.util.List;
  * @author Ferrando Carlos
  */
 public class Baraja {
-    private List <Carta> cartas;
-    private List <Carta> monton;
+    private List<Carta> cartas;
+    private List<Carta> monton;
 
     public Baraja() {
         this.cartas = new ArrayList<>();
         this.monton = new ArrayList<>();
+        inicializarBaraja();
     }
-    
-    
+
+    /* Inicializa la baraja de manera secuencial */
+    private void inicializarBaraja() {
+        String[] palos = { "Espadas", "Bastos", "Oros", "Copas" };
+        for (int numero = 1; numero <= 12; numero++) {
+            if (numero != 8 && numero != 9) {
+                for (String palo : palos) {
+                    cartas.add(new Carta(numero, palo));
+                }
+            }
+        }
+    }
+
+    /* Solo un metodo de prueba para que muestra todas las cartas */
+    public void mostrarPrueba() {
+        for (Carta cart : cartas) {
+            System.out.println(cart);
+        }
+
+    }
+
 }
